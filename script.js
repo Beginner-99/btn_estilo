b4528c82194750f4fbf20c856779c90b9a9d408f
Beginner-99 <paulooramon@gmail.com>
2019-12-23 23:56:46 -0300

    botao feito com css e js

---
+++ script.js
@@ -0,0 +1,8 @@
+function arrowEspaco() {
+    let espaco = document.querySelector("span");
+    if (espaco.style.paddingLeft === "20px") {
+        espaco.style.paddingLeft = "0px";
+    } else {
+        espaco.style.paddingLeft = '20px';
+    }
+}
